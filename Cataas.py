0eb24e03b2177841f58dfbe3879b175c40a81d5e
Olga <olgader4@gmail.com>
2025-07-28 22:16:17 +0300

    Создана функция для получения изображения с котиком

--- Cataas.py
+++ Cataas.py
@@ -1,7 +1,21 @@
+from bottle import response
 from trinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+
+def loade_image():
+    try:
+        response = requests.get(url)  # ответ = запросу с сайта
+        response.raise_for_status()  # обработка исключений
+        image_data = BytesIO(response.content)  #
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
+        return None
+
 
 window = Tk()
 window.title('Cats!')
@@ -11,7 +25,7 @@ label = Label()
 label.pack()
 
 url = 'https://cataas.com/cat'
-img = load_image(url)  # загрузка изображений
+img = loade_image(url)  # загрузка изображений
 
 if img:
     label.config(image=img)
