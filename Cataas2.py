46ccc8f1ca11a180c2dba9930c42b9af7d0b774a
Olga <olgader4@gmail.com>
2025-07-29 17:29:57 +0300

    Добавлен комбобокс для выбора тегов. Поле Enjtry удалено

--- Cataas2.py
+++ Cataas2.py
@@ -1,5 +1,5 @@
 from webbrowser import open_new
-
+from tkinter import ttk
 from bottle import response
 from tkinter import *
 from PIL import Image, ImageTk
@@ -9,6 +9,7 @@ from io import BytesIO
 from gevent.testing.travis import command
 from pygame.display import update
 
+Allowed_tags = ['sleep', 'jump', 'smile', 'fight', 'black', 'white', 'siamese']
 
 def loade_image(url):
     try:
@@ -24,9 +25,9 @@ def loade_image(url):
 
 
 def open_new_window():  # функция открытия изображений каждое в новом окне
-    tag = tag_entry.get()
-    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
-    img = loade_image(url_tag)  # получаем изображения (кладем изобрадение сюда из функции loade_image)
+    tag = tag_combobox.get()   # получаем тег от пользователя
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'  # склеиваем УРЛ и введенный тег, если тег существует. добавили тернарный оператор: если тег пустой, то просто переходим на сайт (иначе / мешается)
+    img = loade_image(url_tag)  # получаем изображения с тегом (кладем изобрадение сюда из функции loade_image)
     if img:
         new_window = Toplevel()  # Создаем новое вторичное окно
         new_window.title('Картинка с котиком')
@@ -36,17 +37,15 @@ def open_new_window():  # функция открытия изображений
         label.image = img
 
 
-def exit():
+def exit_app():
     window.destroy()
 
 window = Tk()
 window.title('Cats!')
 window.geometry('600x480')
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text='Загрузить по тегу', command=open_new_window)
+# tag_entry = Entry()    #поле ввода для тегов
+# tag_entry.pack()
 
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
@@ -58,9 +57,17 @@ file_menu = Menu(menu_bar, tearoff=0)  # чтобы меню не отклеив
 menu_bar.add_cascade(label='Файл', menu=file_menu)
 file_menu.add_command(label='Загрузить фото', command = open_new_window)
 file_menu.add_separator()
-file_menu.add_command(label='Выход', command=exit)
+file_menu.add_command(label='Выход', command=exit_app)
 
 url = 'https://cataas.com/cat'
+tag_label = Label(text='Выбери тег')
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
 
 window.mainloop()
 
