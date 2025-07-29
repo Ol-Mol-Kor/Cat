e36b7d1b1ae73044b2162fd47b71e998dd9f84ce
Olga <olgader4@gmail.com>
2025-07-29 16:05:05 +0300

    Добавили кнопку обновления

--- Cataas.py
+++ Cataas.py
@@ -1,21 +1,29 @@
 from bottle import response
-from trinter import *
+from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from pygame.display import update
 
-def loade_image():
+
+def loade_image(url):
     try:
         response = requests.get(url)  # ответ = запросу с сайта
         response.raise_for_status()  # обработка исключений
-        image_data = BytesIO(response.content)  #
-        img = Image.open(image_data)
-        return ImageTk.PhotoImage(img)
+        image_data = BytesIO(response.content)  #  берем обработанное изображение
+        img = Image.open(image_data)  # превращаем в нормальное изображение
+        return ImageTk.PhotoImage(img)  # возвращаем изображение
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
 
+
+def set_image():  # функция обновления изображений
+    img = loade_image(url)  # загрузка изображений (кладем изобрадение сюда из функции loade_image)
+    if img:
+        label.config(image=img)  # передаем полученное изображение на метку
+        label.image = img
 
 window = Tk()
 window.title('Cats!')
@@ -23,13 +31,12 @@ window.geometry('600x480')
 
 label = Label()
 label.pack()
+update_button = Button(text='Обновить', command=set_image)
+update_button.pack()
 
 url = 'https://cataas.com/cat'
-img = loade_image(url)  # загрузка изображений
 
-if img:
-    label.config(image=img)
-    label.image = img
+set_image()
 
 window.mainloop()
 
